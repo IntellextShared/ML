97ad2e271f4e2e29c0bd8fc3ea503d21bf03f2ec
Demosthen <1008981@gmail.com>
2021-05-06 18:05:11 -0700

    Add Integrated Gradients attribution

---
+++ interpret.py
@@ -0,0 +1,99 @@
+#import libraries
+import transformers
+import argparse
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch.nn.functional as F
+import torch
+import torch.nn as nn
+from captum.attr import LayerIntegratedGradients, TokenReferenceBase, visualization
+
+import pickle
+LABEL_MAP = {
+        0: "NEGATIVE",
+        1: "NEUTRAL",
+        2: "POSITIVE"
+    }
+class WrapperModel(nn.Module):
+    def __init__(self, model):
+        super(WrapperModel, self).__init__()
+        self.model = model
+        self.device=model.device
+
+    def forward(self, *input, **kwargs):
+        out = self.model(*input, **kwargs)
+        return out.logits
+
+class Visualizer():
+    """Uses Integrated Gradients to compute attributions for a sentiment classification"""
+    def __init__(self):
+        # accumalate couple samples in this array for visualization purposes
+        self.vis_data_records_ig = []
+
+    def interpret_sentence(self, model, sentence, tokenizer, min_len = 32):
+        """Uses model to predict the sentiment of a sentence, then uses Integrated Gradients to compute attributions
+            INPUTS:
+            model: sentiment classification model
+            sentence (str): sentence to classify the sentiment of
+            tokenizer: tokenizer associated with our model to convert the sentence into tokens
+            min_len: the length to pad our sentence to
+
+        """
+        model = WrapperModel(model)
+        lig = LayerIntegratedGradients(model, model.model.roberta.embeddings)
+        PAD_IND = tokenizer.pad_token_id
+        token_reference = TokenReferenceBase(reference_token_idx=PAD_IND)
+        indexed = tokenizer([sentence],
+                                padding="max_length",
+                                truncation=True,
+                                max_length=32,
+                                return_tensors="pt")
+        print(indexed.tokens())
+        text = tokenizer.convert_ids_to_tokens(indexed['input_ids'][0])
+
+        if len(text) < min_len:
+            text += ['pad'] * (min_len - len(text))
+
+        model.zero_grad()
+
+        input_indices = indexed['input_ids']
+        
+        # input_indices dim: [sequence_length]
+        seq_length = min_len
+
+        # predict
+        preds = F.softmax(model(**indexed), dim=-1)
+        pred_ind = torch.argmax(preds.squeeze()).item()
+        pred = torch.max(preds)
+        label = pred_ind
+        # generate reference indices for each sample
+        reference_indices = token_reference.generate_reference(seq_length, device=model.device).unsqueeze(0)
+
+        # compute attributions and approximation delta using layer integrated gradients
+        attributions_ig, delta = lig.attribute(input_indices, reference_indices, \
+                                            n_steps=100, return_convergence_delta=True, target=pred_ind)
+
+        print('pred: ', pred.item(), ', delta: ', abs(delta))
+
+        self.add_attributions_to_visualizer(attributions_ig, text, pred, pred_ind, label, delta, self.vis_data_records_ig)
+        return pred, LABEL_MAP[pred_ind], attributions_ig
+        
+    def add_attributions_to_visualizer(self, attributions, text, pred, pred_ind, label, delta, vis_data_records):
+        attributions = attributions.sum(dim=2).squeeze(0)
+        attributions = attributions / torch.norm(attributions)
+        attributions = attributions.cpu().detach().numpy()
+        
+        # storing couple samples in an array for visualization purposes
+        self.vis_data_records_ig.append(visualization.VisualizationDataRecord(
+                                attributions,
+                                pred.detach().cpu().numpy(),
+                                LABEL_MAP[pred_ind],
+                                LABEL_MAP[label],
+                                LABEL_MAP[label],
+                                attributions.sum(),       
+                                text,
+                                delta))
+    
+    def save_visualization(self, path):
+        html = visualization.visualize_text(self.vis_data_records_ig)
+        with open(path, "wb") as file:
+            pickle.dump(html, file)
--- main.py
+++ main.py
@@ -1,18 +1,43 @@
 #import libraries
 import transformers
 import argparse
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch.nn.functional as F
+import torch
+import torch.nn as nn
+from captum.attr import LayerIntegratedGradients, TokenReferenceBase, visualization
+
+import pickle
+from interpret import Visualizer
+LABEL_MAP = {
+        0: "NEGATIVE",
+        1: "NEUTRAL",
+        2: "POSITIVE"
+    }
+
 
 if __name__ == "__main__":
     #Set up command line argument parser
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-                            "-text", 
-                            type=str,
-                            help="Text to do sentiment classification on")
+    parser.add_argument("-text", 
+                        type=str,
+                        help="Text to do sentiment classification on")
+    parser.add_argument("--save_path", 
+                        type=str,
+                        help="Path to save visualization to",
+                        default="viz.pkl")
     args = parser.parse_args()
+
     #Initialize sentiment classifier
-    classifier = transformers.pipeline('sentiment-analysis')
-    #Classify text from command line
-    sentiment = classifier(args.text)[0]
+    model_name = "cardiffnlp/twitter-roberta-base-sentiment"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
+    visualizer = Visualizer()
+
+    #Classify text from command line and compute attributions
+    score, label, atts = visualizer.interpret_sentence(model, args.text, tokenizer)
+    print('Visualize attributions based on Integrated Gradients')
+    visualizer.save_visualization(args.save_path)
+    
     #Output results
-    print("Your sentence had a sentiment of {} with a score of {:.3f}".format(sentiment["label"], sentiment["score"]))
+    print("Your sentence had a sentiment of {} with a score of {:.3f}".format(label, score))
