ae50ca0684ad84e23c53e305551ae8e732138d15
Demosthen <1008981@gmail.com>
2021-06-08 16:21:21 -0700

    Add sample Slack bot integration

---
+++ sentiment_analysis/app.py
@@ -0,0 +1,111 @@
+import os
+import logging
+from flask import Flask
+from slack_sdk.web import WebClient
+from slackeventsapi import SlackEventAdapter
+from message_builder import MessageBuilder
+from main import add_args_to_parser, SentimentAnalyzer
+import argparse
+# Initialize a Flask app to host the events adapter
+app = Flask(__name__)
+slack_events_adapter = SlackEventAdapter(os.environ["SLACK_SIGNING_SECRET"], "/slack/events", app)
+
+# Initialize a Web API client
+slack_web_client = WebClient(token=os.environ['SLACK_BOT_TOKEN'])
+
+# For simplicity we'll store our app data in-memory with the following data structure.
+# onboarding_tutorials_sent = {"channel": {"user_id": OnboardingTutorial}}
+onboarding_tutorials_sent = {}
+messages = []
+
+parser = argparse.ArgumentParser()
+parser = add_args_to_parser(parser)
+analysis_args = parser.parse_args()
+SA = SentimentAnalyzer(analysis_args)
+
+def start_onboarding(user_id: str, channel: str):
+    # Create a new onboarding tutorial.
+    onboarding_tutorial = MessageBuilder(channel)
+
+    # Get the onboarding message payload
+    message = onboarding_tutorial.get_message_payload()
+
+    # Post the onboarding message in Slack
+    response = slack_web_client.chat_postMessage(**message)
+
+    # Capture the timestamp of the message we've just posted so
+    # we can use it to update the message after a user
+    # has completed an onboarding task.
+    onboarding_tutorial.timestamp = response["ts"]
+
+    # Store the message sent in onboarding_tutorials_sent
+    if channel not in onboarding_tutorials_sent:
+        onboarding_tutorials_sent[channel] = onboarding_tutorial
+
+
+# ================ Team Join Event =============== #
+# When the user first joins a team, the type of the event will be 'team_join'.
+# Here we'll link the onboarding_message callback to the 'team_join' event.
+@slack_events_adapter.on("team_join")
+def onboarding_message(payload):
+    """Create and send an onboarding welcome message to new users. Save the
+    time stamp of this message so we can update this message in the future.
+    """
+    event = payload.get("event", {})
+
+    # Get the id of the Slack user associated with the incoming event
+    user_id = event.get("user", {}).get("id")
+
+    # Open a DM with the new user.
+    response = slack_web_client.conversations_open(users=user_id)
+    channel = response["channel"]["id"]
+
+    # Post the onboarding message.
+    #start_onboarding(user_id, channel)
+
+# ============== Message Events ============= #
+# When a user sends a DM, the event type will be 'message'.
+# Here we'll link the message callback to the 'message' event.
+@slack_events_adapter.on("message")
+def message(payload):
+    """Display the onboarding welcome message after receiving a message
+    that contains "start".
+    """
+    event = payload.get("event", {})
+
+    channel_id = event.get("channel")
+    user_id = event.get("user")
+    text = event.get("text")
+
+
+    if text and text.lower() == "start":
+        print("Starting onboarding...")
+        return start_onboarding(user_id, channel_id)
+    elif text and channel_id in onboarding_tutorials_sent:
+        # Get the original tutorial sent.
+        onboarding_tutorial = onboarding_tutorials_sent[channel_id]
+        if onboarding_tutorial.userid == user_id:
+            return
+
+        # Mark the pin task as completed.
+        onboarding_tutorial.last_msg = text
+
+        messages.append(text)
+        SA.set_text(messages)
+        print("RUNNING ANALYSIS...")
+        topic_sentiments = SA.run_analysis()
+        print("ANALYSIS COMPLETE")
+        onboarding_tutorial.topic_sentiments = topic_sentiments
+        # Get the new message payload
+        message = onboarding_tutorial.get_message_payload()
+
+        # Post the updated message in Slack
+        updated_message = slack_web_client.chat_postMessage(**message)
+
+
+
+if __name__ == "__main__":
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(logging.StreamHandler())
+    app.run(port=3000)
--- sentiment_analysis/main.py
+++ sentiment_analysis/main.py
@@ -18,9 +18,15 @@ LABEL_MAP = {
     }
 
 def add_args_to_parser(parser):
-    parser.add_argument("-text_file", 
+    
+    parser.add_argument("--text_file", 
                         type=str,
-                        help="File with text to do sentiment classification on")
+                        help="File with text to do sentiment classification on",
+                        default=None)
+    parser.add_argument("--text",
+                        type=list,
+                        help="List of strings to do topic and sentiment analysis on (to be set in app.py, not directly from terminal)",
+                        default=None)
     parser.add_argument("--save_path", 
                         type=str,
                         help="Path to save visualization to",
@@ -52,46 +58,63 @@ def sort_by_topic(text, topic_labels, topics):
             sorted_text[label] += text[i] + " "
     return sorted_text
 
+class SentimentAnalyzer():
+    def __init__(self, args):
+        self.args = args
+        #Initialize sentiment classifier
+        self.model_name = "cardiffnlp/twitter-roberta-base-sentiment"
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
+        self.model = AutoModelForSequenceClassification.from_pretrained(self.model_name)
+        self.sentiment_classifier = InterpretableSentimentClassifier()
+
+        #Initialize zero-shot topic classifier
+        self.topic_classifier = pipeline("zero-shot-classification",
+                            model="valhalla/distilbart-mnli-12-9")    
+
+    def set_text(self, text):
+        self.args.text = text
+
+    def run_analysis(self):
+        assert self.args.text_file or self.args.text
+        if self.args.text_file:
+            text = read_file(self.args.text_file)
+        elif self.args.text:
+            text = self.args.text
+
+        #Classify topic of text from command line
+        topic_out = self.topic_classifier(text, self.args.topics, multi_label=True)
+        topics = topic_out[0]["labels"]
+
+        topic_labels = [[topic_out[i]["labels"][j] for j in range(len(topics)) if topic_out[i]["scores"][j] > 0.7] for i in range(len(topic_out))]
+        
+
+        sorted_text = sort_by_topic(text, topic_labels, topics)
+        topic_sentiments = {}
+        with open("out.txt", "w") as file:
+            # Process sentiment for each topic
+            for topic, sentences in sorted_text.items():
+                if self.args.interpret:
+                    #Classify sentiment of text from command line and compute attributions
+                    score, label, atts = self.sentiment_classifier.interpret_sentence(self.model, sentences, self.tokenizer)
+                    print('Visualize attributions based on Integrated Gradients')
+                    self.sentiment_classifier.save_visualization(self.args.save_path)
+                else:
+                    score, label, _ = self.sentiment_classifier.classify_sentiment(self.model, sentences, self.tokenizer)
+                topic_sentiments[topic] = (label, score, sentences)
+
+                #Output results
+                file.write("{} SENTIMENT: {} SCORE {} TOPIC: {} \n".format(sentences, label, score, topic))
+        return topic_sentiments
+
+    #print("Your sentence had a sentiment of {} with a score of {:.3f} and a topic of {}".format(label, score, topic))
+
 if __name__ == "__main__":
     #Set up command line argument parser
     parser = argparse.ArgumentParser()
     parser = add_args_to_parser(parser)
     
     args = parser.parse_args()
+    SA = SentimentAnalyzer(args)
+    SA.run_analysis()
 
-    text = read_file(args.text_file)
-
-    #Initialize sentiment classifier
-    model_name = "cardiffnlp/twitter-roberta-base-sentiment"
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForSequenceClassification.from_pretrained(model_name)
-    sentiment_classifier = InterpretableSentimentClassifier()
-
-    #Initialize zero-shot topic classifier
-    topic_classifier = pipeline("zero-shot-classification",
-                        model="facebook/bart-large-mnli")    
-
-    #Classify topic of text from command line
-    topic_out = topic_classifier(text, args.topics, multi_label=True)
-    topics = topic_out[0]["labels"]
-
-    topic_labels = [[topic_out[i]["labels"][j] for j in range(len(topics)) if topic_out[i]["scores"][j] > 0.7] for i in range(len(topic_out))]
     
-
-    sorted_text = sort_by_topic(text, topic_labels, topics)
-
-    with open("out.txt", "w") as file:
-        # Process sentiment for each topic
-        for topic, sentences in sorted_text.items():
-            if args.interpret:
-                #Classify sentiment of text from command line and compute attributions
-                score, label, atts = sentiment_classifier.interpret_sentence(model, sentences, tokenizer)
-                print('Visualize attributions based on Integrated Gradients')
-                sentiment_classifier.save_visualization(args.save_path)
-            else:
-                score, label, _ = sentiment_classifier.classify_sentiment(model, sentences, tokenizer)
-
-            #Output results
-            file.write("{} SENTIMENT: {} SCORE {} TOPIC: {} \n".format(sentences, label, score, topic))
-
-    #print("Your sentence had a sentiment of {} with a score of {:.3f} and a topic of {}".format(label, score, topic))
